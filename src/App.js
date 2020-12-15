afa5d5aee5ecd50364d46a31706ce7c33c4c9983
n1ckk007 <antonellos101@gmail.com>
2020-12-15 23:56:23 +1100

    styled about section and global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+import GlobalStyle from "./GlobalStyle";
 import AboutUsPage from "./pages/AboutUsPage";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUsPage />
     </div>
   );
--- src/components/about-section/AboutSection.js
+++ src/components/about-section/AboutSection.js
@@ -1,34 +1,40 @@
 import React from "react";
 import home1 from "../../img/home1.png";
+import {
+  AboutContainer,
+  Description,
+  Hide,
+  ImageContainer,
+} from "./AboutSectionStyles";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <AboutContainer>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>Lorem ipsum dolor sit.</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               {" "}
               Lorem <span>ipsum</span> dolor
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>Lorem</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
           perspiciatis aspernatur harum quas consequatur.
         </p>
         <button>Contact Me</button>
-      </div>
-      <div className="image">
+      </Description>
+      <ImageContainer>
         <img src={home1} alt="Guy with a camera" />
-      </div>
-    </div>
+      </ImageContainer>
+    </AboutContainer>
   );
 };
 
