92530a93efa726e1243379321bbbec0dde62f53e
n1ckk007 <antonellos101@gmail.com>
2020-12-18 00:25:43 +1100

    styled services section and added new font

---
+++ src/components/services-section/ServicesSectionStyles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const ServicesContainer = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0 4rem 0;
+  }
+`;
+
+export const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+`;
+
+export const ImageContainer = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+export const Hide = styled.div`
+  overflow: hidden;
+`;
+
+export const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
